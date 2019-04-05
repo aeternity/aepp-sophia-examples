88fb8f5257dd7e14559e807e8cc922f947bc2a5a
Martin Grigorov <maptuhec@gmail.com>
2019-04-05 16:37:02 +0300

    add exchange market and oracles smart contracs examples

--- examples/ExchangeOracles/test/exchangeOracles.js
+++ examples/ExchangeOracles/test/exchangeOracles.js
@@ -12,9 +12,9 @@ const crypto = AeSDK.Crypto;
 const assert = chai.assert;
 chai.use(chaiAsPromised);
 const ttl = 1000
-const qfee = 10
-const _aePrice = 1000
-const _tokenPrice = 1500
+const qfee = 50
+const _aePrice = 100
+const _tokenPrice = 150
 const _updatedAePrice = 2000
 const _updatedTokenPrice = 2000
 const _zeroPrice = 0
@@ -31,37 +31,37 @@ describe('ExchangeOracle', () => {
 	before(async () => {
 
 		//Test net configuration
-		firstClient = await Universal({
-			url: "https://sdk-testnet.aepps.com",
-			internalUrl: "https://sdk-testnet.aepps.com",
-			keypair: config.ownerKeyPairTest,
-			nativeMode: true,
-			networkId: 'ae_uat'
-		});
-		secondClient = await Universal({
-			url: "https://sdk-testnet.aepps.com",
-			internalUrl: "https://sdk-testnet.aepps.com",
-			keypair: config.notOwnerKeyPair,
-			nativeMode: true,
-			networkId: 'ae_uat'
-		});
+		// firstClient = await Universal({
+		// 	url: "https://sdk-testnet.aepps.com",
+		// 	internalUrl: "https://sdk-testnet.aepps.com",
+		// 	keypair: config.ownerKeyPairTest,
+		// 	nativeMode: true,
+		// 	networkId: 'ae_uat'
+		// });
+		// secondClient = await Universal({
+		// 	url: "https://sdk-testnet.aepps.com",
+		// 	internalUrl: "https://sdk-testnet.aepps.com",
+		// 	keypair: config.notOwnerKeyPair,
+		// 	nativeMode: true,
+		// 	networkId: 'ae_uat'
+		// });
 
 
 		//Local node configuration
-		// firstClient = await Universal({
-		// 	url: config.host,
-		// 	internalUrl: config.internalHost,
-		// 	keypair: config.ownerKeyPair,
-		// 	nativeMode: true,
-		// 	networkId: 'ae_devnet'
-		// });
-		// secondClient = await Universal({
-		// 	url: config.host,
-		// 	internalUrl: config.internalHost,
-		// 	keypair: config.notOwnerKeyPair,
-		// 	nativeMode: true,
-		// 	networkId: 'ae_devnet'
-		// });
+		firstClient = await Universal({
+			url: config.host,
+			internalUrl: config.internalHost,
+			keypair: config.ownerKeyPair,
+			nativeMode: true,
+			networkId: 'ae_devnet'
+		});
+		secondClient = await Universal({
+			url: config.host,
+			internalUrl: config.internalHost,
+			keypair: config.notOwnerKeyPair,
+			nativeMode: true,
+			networkId: 'ae_devnet'
+		});
 
 
 		// firstClient.setKeypair(config.ownerKeyPair)
@@ -72,28 +72,26 @@ describe('ExchangeOracle', () => {
 
 	})
 
-	it.only('deploying oracle successfully', async () => {
+	it('deploying oracle successfully', async () => {
 		//Arrange
+
 		const compiledContract = await firstClient.contractCompile(oracleSource, {})
-		let nonce = await firstClient.api.getAccountByPubkey(await firstClient.address())
-		console.log(nonce)
 
 		//Act
 		const deployedContract = await compiledContract.deploy({
 			initState: `(${qfee}, ${ttl})`,
 			options: {
 				ttl: config.ttl,
-				nonce: nonce.nonce + 2
+				verify: true
 			}
 		});
 
 		//Assert
 		let ownerAddress = await firstClient.address();
-		console.log(deployedContract)
 		assert.equal(ownerAddress, deployedContract.owner)
 	})
 
-	xit('deploying market successfully', async () => {
+	it('deploying market successfully', async () => {
 		//Arrange
 		const compiledContract = await firstClient.contractCompile(marketSource, {
 			gas: config.gas
@@ -109,46 +107,9 @@ describe('ExchangeOracle', () => {
 		});
 
 		//Assert
-		assert.equal(config.ownerKeyPair.publicKey, deployedContract.owner)
+		let ownerAddress = await firstClient.address();
+		assert.equal(ownerAddress, deployedContract.owner)
 	})
-
-
-	xit('should throw when trying to deploy oracle with zero qfee', async () => {
-		//Arrange
-		const compiledContract = await firstClient.contractCompile(oracleSource, {})
-
-		//Act
-		const deployedContract = await compiledContract.deploy({
-			initState: `(${0}, ${ttl})`,
-			options: {
-				ttl: config.ttl
-			}
-		});
-
-		//Assert
-		assert.isRejected(deployedContract, 0, "Deploying oracle contract without zero qfee succeeded")
-	})
-
-	xit('should throw when trying to deploy oracle with zero ttl', async () => {
-		//Arrange
-		const compiledContract = await firstClient.contractCompile(oracleSource, {})
-
-		//Act
-		const deployedContract = await compiledContract.deploy({
-			initState: `(${qfee}, ${0})`,
-			options: {
-				ttl: config.ttl
-			}
-		});
-
-		//Assert
-		console.log(deployedContract.owner, "TEST")
-
-		assert.isRejected(deployedContract, 0, "Deploying oracle contract without zero ttl succeeded")
-
-	})
-
-
 
 	describe('Oracle smart contract tests', () => {
 
@@ -173,9 +134,6 @@ describe('ExchangeOracle', () => {
 		})
 
 		it('should register an oracle successfully  ', async () => {
-			console.log("TEST2");
-
-			console.log(deployedOracleContract)
 			let oracleAddress = await firstClient.contractCallStatic(deployedOracleContract.address, 'sophia-address', 'getOracle', {})
 			let oralceData = await oracleAddress.decode('int')
 			assert.notEqual(oralceData.value, "", 'Registering the oracle has failed');
@@ -186,7 +144,7 @@ describe('ExchangeOracle', () => {
 		it('should make a query from the oracle contract', async () => {
 			let string = "aePrice"
 			let queryTtl = 50
-			let queryFee = 20
+			let queryFee = 70
 
 			let createQueryPromise = await deployedOracleContract.call('createQuery', {
 				args: `("${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
@@ -196,7 +154,6 @@ describe('ExchangeOracle', () => {
 				},
 				abi: "sophia"
 			});
-			console.log(createQueryPromise);
 
 			let queryData = await createQueryPromise.decode('int')
 			assert.notEqual(queryData.value, "", 'Querying the oracle has failed');
@@ -207,7 +164,7 @@ describe('ExchangeOracle', () => {
 
 			let question = "aePrice"
 			let queryTtl = 50
-			let queryFee = 20
+			let queryFee = 70
 
 			let createQueryPromise = await deployedOracleContract.call('createQuery', {
 				args: `("${question}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
@@ -233,35 +190,40 @@ describe('ExchangeOracle', () => {
 		it("should respond to question", async () => {
 			let string = "aePrice"
 			let queryTtl = 50
-			let queryFee = 20
+			let queryFee = 70
 			let answer = 100
 
 			let createQueryPromise = await deployedOracleContract.call('createQuery', {
 				args: `("${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
 				options: {
-					ttl: config.ttl
+					ttl: config.ttl,
+					verify: true
 				},
 				abi: "sophia"
 			});
 
 			let queryData = await createQueryPromise.decode('int')
+			let respondToQuestionPromise
 
-			let respondToQuestionPromise = deployedOracleContract.call('respondToQuestion', {
+			respondToQuestionPromise = deployedOracleContract.call('respondToQuestion', {
 				args: `(${queryData.value}, ${answer})`,
 				options: {
-					ttl: config.ttl
+					ttl: config.ttl,
+					verify: true,
+					fee: 203418000000000000
 				},
 				abi: "sophia"
 			});
 
+
 			assert.isFulfilled(respondToQuestionPromise, "The response is not working")
-			await respondToQuestionPromise;
+			await respondToQuestionPromise
 		})
 
 		it('should make a query from the oracle contract', async () => {
 			let string = "aePrice"
 			let queryTtl = 50
-			let queryFee = 20
+			let queryFee = 70
 
 			let createQueryPromise = await deployedOracleContract.call('createQuery', {
 				args: `("${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
@@ -279,12 +241,13 @@ describe('ExchangeOracle', () => {
 
 	})
 
-	xdescribe('Market smart contract tests', () => {
+	describe('Exchange Market smart contract tests', () => {
 
 		let deployedOracleContract;
 		let compiledOracleContract;
 		let deployedMarketContract;
 		let compiledMarketContract;
+		let oracleId
 
 		beforeEach(async () => {
 			compiledOracleContract = await firstClient.contractCompile(oracleSource, {})
@@ -295,7 +258,8 @@ describe('ExchangeOracle', () => {
 				options: {
 					ttl: config.ttl,
 					amount: 100000,
-					deposit: 100000
+					deposit: 100000,
+					verify: true
 				},
 				abi: "sophia"
 			});
@@ -306,10 +270,15 @@ describe('ExchangeOracle', () => {
 				options: {
 					ttl: config.ttl,
 					deposit: 100000,
-					amount: 100000
+					amount: 100000,
+					verify: true
 				},
 				abi: "sophia"
 			});
+
+			let oracleAddress = await firstClient.contractCallStatic(deployedOracleContract.address, 'sophia-address', 'getOracle', {})
+			let oracleData = await oracleAddress.decode('int')
+			oracleId = oracleData.value
 		})
 		it("should get the query_fee", async () => {
 
@@ -353,7 +322,6 @@ describe('ExchangeOracle', () => {
 
 		it("should update the token price", async () => {
 
-
 			const updatingTokenPricePromise = await deployedMarketContract.call('updateTokenPrice', {
 				args: `(${_updatedTokenPrice})`,
 				options: {
@@ -371,6 +339,101 @@ describe('ExchangeOracle', () => {
 
 			let finalTokenPrice = await getTokenPricePromise.decode("int")
 			assert.equal(finalTokenPrice.value, _updatedTokenPrice, "Token price was not updated properly")
+
+		})
+
+		it("should createa query from the market", async () => {
+
+			let string = "aePrice"
+			let queryTtl = 50
+			let queryFee = 70
+
+			let createQueryPromise = await deployedMarketContract.call('createQuery', {
+				args: `(${oracleId},"${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			let queryData = await createQueryPromise.decode('int')
+			assert.notEqual(queryData.value, "", 'Querying the oracle has failed');
+		})
+
+		it("should create query for AE Price from the market", async () => {
+
+			let queryTtl = 50
+			let queryFee = 70
+
+			let createQueryPromise = await deployedMarketContract.call('createAePriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			let queryData = await createQueryPromise.decode('int')
+			assert.notEqual(queryData.value, "", 'Querying the oracle has failed');
+		})
+		it("should create query for Token price from the market", async () => {
+
+			let queryTtl = 50
+			let queryFee = 70
+
+			let createQueryPromise = await deployedMarketContract.call('createTokenPriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			let queryData = await createQueryPromise.decode('int')
+			assert.notEqual(queryData.value, "", 'Querying the oracle has failed');
+		})
+
+		it("should get the answer to a question", async () => {
+
+			let string = "aePrice"
+			let queryTtl = 50
+			let queryFee = 70
+			let answer = 100
+
+			let createQueryPromise = await deployedOracleContract.call('createQuery', {
+				args: `("${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			let queryData = await createQueryPromise.decode('int')
+
+			let respondToQuestionPromise = await deployedOracleContract.call('respondToQuestion', {
+				args: `(${queryData.value}, ${answer})`,
+				options: {
+					ttl: config.ttl,
+					verify: true,
+					fee: 204418000000000000
+				},
+				abi: "sophia"
+			});
+			let getAnswerResult = await deployedMarketContract.call('getAnswer', {
+				args: `(${oracleId}, ${queryData.value})`,
+				options: {
+					ttl: config.ttl,
+					fee: 204418000000000000,
+					verify: true
+				},
+				abi: "sophia"
+			});
+			let answerData = await getAnswerResult.decode('int')
+			assert.equal(answerData.value, answer, "The answer is not correct")
 
 		})
 
@@ -400,6 +463,162 @@ describe('ExchangeOracle', () => {
 
 			await assert.isRejected(updatingAePricePromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1RoZSBwcmljZSBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACO03Cq", "Update price not failing with zero value")
 
+		})
+
+		it("should throw when creating a query with 0 qfee", async () => {
+
+			let string = "aePrice"
+			let queryTtl = 50
+			let queryFee = 0
+
+			let createQueryPromise = deployedMarketContract.call('createQuery', {
+				args: `(${oracleId},"${string}", ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IGZlZSBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABGMQUi", "Create Query not failing with zero value for qfee")
+		})
+
+		it("should throw when creating a query with 0 qttl", async () => {
+
+			let string = "aePrice"
+			let queryTtl = 0
+			let rttl = 100
+			let queryFee = 10
+
+			let createQueryPromise = deployedMarketContract.call('createQuery', {
+				args: `(${oracleId},"${string}", ${queryFee}, ${queryTtl}, ${rttl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADDFfMF", "Create Query not failing with zero value for qttl")
+		})
+
+		it("should throw when creating a query with 0 rttl", async () => {
+
+			let string = "aePrice"
+			let queryTtl = 100
+			let rttl = 0
+			let queryFee = 10
+
+			let createQueryPromise = deployedMarketContract.call('createQuery', {
+				args: `(${oracleId},"${string}", ${queryFee}, ${queryTtl}, ${rttl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJlJlbGF0aXZlIFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADnxm7C", "Create Query not failing with zero value for rttl")
+		})
+
+		it("should throw when creating query for AE Price from the market with 0 qfee", async () => {
+
+			let queryTtl = 50
+			let queryFee = 0
+
+			let createQueryPromise = deployedMarketContract.call('createAePriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IGZlZSBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABGMQUi", "Create Query for Ae Price not failing with zero value for qfee")
+		})
+		it("should throw when creating query for AE Price from the market with 0 qttl", async () => {
+
+			let queryTtl = 0
+			let queryFee = 100
+
+			let createQueryPromise = deployedMarketContract.call('createAePriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADDFfMF", "Create Query for Ae Price not failing with zero value for qttl")
+		})
+
+		it("should throw when creating query for AE Price from the market with 0 qfee", async () => {
+
+			let queryTtl = 50
+			let queryFee = 100
+
+			let createQueryPromise = deployedMarketContract.call('createAePriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${0})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJlJlbGF0aXZlIFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADnxm7C", "Create Query for Ae Price not failing with zero value for rttl")
+		})
+
+		it("should throw when creating query for Token Price from the market with 0 qfee", async () => {
+
+			let queryTtl = 50
+			let queryFee = 0
+
+			let createQueryPromise = deployedMarketContract.call('createTokenPriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IGZlZSBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABGMQUi", "Create Query for Token Price not failing with zero value for qfee")
+		})
+		it("should throw when creating query for Token Price from the market with 0 qttl", async () => {
+
+			let queryTtl = 0
+			let queryFee = 100
+
+			let createQueryPromise = deployedMarketContract.call('createTokenPriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${queryTtl})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAI1F1ZXJ5IFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADDFfMF", "Create Query for Token Price not failing with zero value for qttl")
+		})
+
+		it("should throw when creating query for Token Price from the market with 0 qfee", async () => {
+
+			let queryTtl = 50
+			let queryFee = 100
+
+			let createQueryPromise = deployedMarketContract.call('createTokenPriceQuery', {
+				args: `(${oracleId}, ${queryFee}, ${queryTtl}, ${0})`,
+				options: {
+					ttl: config.ttl,
+					verify: true
+				},
+				abi: "sophia"
+			});
+
+			await assert.isRejected(createQueryPromise, "_AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJlJlbGF0aXZlIFRUTCBtdXN0IGJlIGdyZWF0ZXIgdGhhbiB6ZXJvAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADnxm7C", "Create Query for Token Price not failing with zero value for rttl")
 		})
 	})
 
