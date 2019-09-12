2d28271b4e1841e3e43274bcf36a03fbef4c0932
Milen Radkov <milenradkov@me.com>
2019-09-12 10:14:56 +0300

    (tic-tac-toe) fix tests

--- examples/TicTacToe/test/ticTacToeTest.js
+++ examples/TicTacToe/test/ticTacToeTest.js
@@ -1,179 +1,71 @@
-const Ae = require('@aeternity/aepp-sdk').Universal
-const Crypto = require('@aeternity/aepp-sdk').Crypto
+const path = require('path');
+const chai = require('chai');
+let chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const assert = chai.assert;
 
-const config = {
-  host: 'http://localhost:3001/',
-  internalHost: 'http://localhost:3001/internal/',
-  gas: 200000,
-  ttl: 55
-}
+const AeSDK = require('@aeternity/aepp-sdk');
+const Crypto = require('@aeternity/aepp-sdk').Crypto;
+const Universal = AeSDK.Universal;
+const config = require("./constants/config.json")
+const utils = require('./utils/utils');
+const getClient = utils.getClient;
 
-async function deployContract (contractName, ...params) {
-  const [owner, gas, deployObj] = [params[0], params[1], params[2]]
-  const contractSource = utils.readFileRelative(
-    `./contracts/${contractName}.aes`,
-    'utf-8'
-  )
-  const compileContract = await owner.contractCompile(contractSource, gas)
-  const deployPromiseContract = await compileContract.deploy(deployObj)
-  return deployPromiseContract
-}
+const TIC_TAC_TOE_CONTRACT_FILE_PATH = "./../contracts/tic-tac-toe.aes";
 
-function decodeContractAddress (contract) {
-  const decoded58addres = Crypto.decodeBase58Check(
-    contract.address.split('_')[1]
-  ).toString('hex')
-  return `0x${decoded58addres}`
-}
+const ticTacToeContractSource = utils.readFileRelative(`./contracts/${TIC_TAC_TOE_CONTRACT_FILE_PATH}`, 'utf-8')
 
-async function callContract (
-  contract,
-  functionName,
-  args,
-  decodeType = 'string'
-) {
-  const result = await contract.call(functionName, args)
-  const decodedResult = await result.decode(decodeType)
-  return decodedResult.value
-}
+describe('Contracts', () => {
+  
+  let client, ticTacToeContractInstance;
 
-describe('TicTacToe Contract', () => {
-  let owner
-  let TicTacToeContract
+  beforeEach(async () => {
+      client = await getClient(Universal, config, config.ownerKeyPair);
+  });
 
-  before(async () => {
-    const ownerKeyPair = wallets[0]
-    owner = await Ae({
-      url: config.host,
-      internalUrl: config.internalHost,
-      keypair: ownerKeyPair,
-      nativeMode: true,
-      networkId: 'ae_devnet'
-    })
-  })
+  describe('Deploy contracts', () => {
 
-  describe('Deploy contract', () => {
-    it('should deploy TicTacToe contract', async () => {
-      const gas = { gas: config.gas }
-      const deployObj = { options: { ttl: config.ttl } }
-      TicTacToeContract = await deployContract(
-        'TicTacToe',
-        owner,
-        gas,
-        deployObj
-      )
-      assert(TicTacToeContract.hasOwnProperty('address'))
-      assert(TicTacToeContract.hasOwnProperty('owner'))
+    it('should deploy Tic Tac Toe contract', async () => {
+      ticTacToeContractInstance = await client.getContractInstance(ticTacToeContractSource);
+      const init = await ticTacToeContractInstance.deploy([]);
+      assert.equal(init.result.returnType, 'ok');
     })
   })
 
   describe('Interact with the contract', () => {
-    before(() => {
-      addressTicTacToe = decodeContractAddress(TicTacToeContract)
-    })
-
     it('should set player 1 ', async () => {
-      const args = {
-        args: `(11,1)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
+      const result = await ticTacToeContractInstance.methods.make_move(11,1)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
     })
 
     it('should set player 2 ', async () => {
-      const args = {
-        args: `(12, 2)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
-    })
-    it('should set player 1 ', async () => {
-      const args = {
-        args: `(33,1)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
-    })
-
-    it('should set player 2 ', async () => {
-      const args = {
-        args: `(23,2)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
+      const result = await ticTacToeContractInstance.methods.make_move(12,2)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
     })
 
     it('should set player 1 ', async () => {
-      const args = {
-        args: `(31, 1)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
+      const result = await ticTacToeContractInstance.methods.make_move(33,1)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
     })
 
     it('should set player 2 ', async () => {
-      const args = {
-        args: `(21,2)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, "Game continues. The other player's turn.")
+      const result = await ticTacToeContractInstance.methods.make_move(23,2)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
+    })
+
+    it('should set player 1 ', async () => {
+      const result = await ticTacToeContractInstance.methods.make_move(31,1)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
+    })
+
+    it('should set player 2 ', async () => {
+      const result = await ticTacToeContractInstance.methods.make_move(21,2)
+      assert.equal(result.decodedResult, "Game continues. The other player's turn.")
     })
 
     it('should win player 1 ', async () => {
-      const args = {
-        args: `(32,1)`,
-        options: { ttl: 55 },
-        abi: 'sophia'
-      }
-      const result = await callContract(
-        TicTacToeContract,
-        'make_move',
-        args,
-        'string'
-      )
-      assert.equal(result, 'You are the winner! Congratulations player 1')
+      const result = await ticTacToeContractInstance.methods.make_move(32,1)
+      assert.equal(result.decodedResult, "You are the winner! Congratulations player 1")
     })
   })
 })
